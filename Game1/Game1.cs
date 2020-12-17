671d6a87fb75316920ea622807228f23284d2923
Jack Dobie <57402187+JackDobie@users.noreply.github.com>
2020-12-17 17:14:29 +0000

    added word check and replace

--- Game1/Game1.cs
+++ Game1/Game1.cs
@@ -2,6 +2,8 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Game1
 {
@@ -11,12 +13,17 @@ namespace Game1
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        List<Texture2D> hangman = new List<Texture2D>();
+        List<Texture2D> hangmanTex = new List<Texture2D>();
         Rectangle hangmanRectangle = new Rectangle(new Point(74, 74), new Point(453));
         int hangmanState;
 
         KeyboardState keyState;
         KeyboardState oldKeyState;
+        List<Keys> keysPressed = new List<Keys>();
+
+        string correctWord;
+        string displayedWord;
+        string displayedWord2;
 
         public Game1()
         {
@@ -27,11 +34,14 @@ namespace Game1
             IsMouseVisible = true;
 
             hangmanState = 0;
+            correctWord = "hello";
+            displayedWord += new string('_', correctWord.Length);
+            displayedWord2 = string.Join(" ", displayedWord.Reverse()); //turns word into _ _ _ _
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            Window.TextInput += TextInputHandler;
 
             base.Initialize();
         }
@@ -40,23 +50,20 @@ namespace Game1
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
-            hangman.Add(this.Content.Load<Texture2D>("hangman/square"));
-            hangman.Add(this.Content.Load<Texture2D>("hangman/hangman-1"));
-            hangman.Add(this.Content.Load<Texture2D>("hangman/hangman-2"));
-            hangman.Add(this.Content.Load<Texture2D>("hangman/hangman-3"));
-            hangman.Add(this.Content.Load<Texture2D>("hangman/hangman-4"));
-            hangman.Add(this.Content.Load<Texture2D>("hangman/hangman-5"));
-            hangman.Add(this.Content.Load<Texture2D>("hangman/hangman-6"));
-            hangman.Add(this.Content.Load<Texture2D>("hangman/hangman-7"));
-            hangman.Add(this.Content.Load<Texture2D>("hangman/hangman-8"));
-            hangman.Add(this.Content.Load<Texture2D>("hangman/hangman-9"));
+            hangmanTex.Add(this.Content.Load<Texture2D>("hangman/square"));
+            hangmanTex.Add(this.Content.Load<Texture2D>("hangman/hangman-1"));
+            hangmanTex.Add(this.Content.Load<Texture2D>("hangman/hangman-2"));
+            hangmanTex.Add(this.Content.Load<Texture2D>("hangman/hangman-3"));
+            hangmanTex.Add(this.Content.Load<Texture2D>("hangman/hangman-4"));
+            hangmanTex.Add(this.Content.Load<Texture2D>("hangman/hangman-5"));
+            hangmanTex.Add(this.Content.Load<Texture2D>("hangman/hangman-6"));
+            hangmanTex.Add(this.Content.Load<Texture2D>("hangman/hangman-7"));
+            hangmanTex.Add(this.Content.Load<Texture2D>("hangman/hangman-8"));
+            hangmanTex.Add(this.Content.Load<Texture2D>("hangman/hangman-9"));
         }
 
         protected override void Update(GameTime gameTime)
         {
-            // TODO: Add your update logic here
-
             keyState = Keyboard.GetState();
 
             if(GetKeyDown(Keys.Space))
@@ -64,7 +71,7 @@ namespace Game1
                 AdvanceHangman();
             }
 
-            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (keyState.IsKeyDown(Keys.Escape))
                 Exit();
 
             oldKeyState = keyState;
@@ -75,22 +82,16 @@ namespace Game1
         {
             GraphicsDevice.Clear(Color.White);
 
-            // TODO: Add your drawing code here
             _spriteBatch.Begin();
 
-            _spriteBatch.Draw(hangman[hangmanState], hangmanRectangle, Color.White);
-
-            //foreach (Texture2D tex in hangman)
-            //{
-            //    _spriteBatch.Draw(tex,hangmanRectangle,Color.White);
-            //}
+            _spriteBatch.Draw(hangmanTex[hangmanState], hangmanRectangle, Color.White);
 
             _spriteBatch.End();
 
             base.Draw(gameTime);
         }
 
-        //returns true the first frame the key is pressed
+        /// <summary> Returns true the first frame the key is pressed </summary>
         bool GetKeyDown(Keys key)
         {
             if (oldKeyState.IsKeyUp(key) && keyState.IsKeyDown(key))
@@ -101,7 +102,7 @@ namespace Game1
                 return false;
         }
 
-        //returns true the first frame the key is released
+        /// <summary> Returns true the first frame the key is released </summary>
         bool GetKeyUp(Keys key)
         {
             if (oldKeyState.IsKeyDown(key) && keyState.IsKeyUp(key))
@@ -117,6 +118,36 @@ namespace Game1
             if(hangmanState < 9)
             {
                 hangmanState++;
+            }
+        }
+
+        void TextInputHandler(object sender, TextInputEventArgs args)
+        {
+            var pressedKey = args.Key;
+            var character = args.Character;
+
+            if(!keysPressed.Contains(pressedKey))
+            {
+                keysPressed.Add(pressedKey);
+
+                StringBuilder sb = new StringBuilder(displayedWord);
+                for (int i = 0; i < correctWord.Length; i++)
+                {
+                    if (correctWord.Substring(i, 1) == character.ToString())
+                    {
+                        sb[i] = character;
+                    }
+                }
+                displayedWord = sb.ToString();
+
+                //if(correctWord.Contains(character))
+                //{
+                //    StringBuilder sb = new StringBuilder(displayedWord);
+                //    sb[correctWord.IndexOf(character)] = character;
+                //    displayedWord = sb.ToString();
+                //}
+
+                AdvanceHangman();
             }
         }
     }
